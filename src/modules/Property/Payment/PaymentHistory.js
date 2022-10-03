dab89796e71410bc30ac1c2ef5b01519faa121f7
Derrick Yeboah <derrickyebaoh999@hotmail.com>
2022-10-03 13:06:56 +0000

    sdded propd

---
+++ src/modules/Property/Payment/PaymentHistory.js
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const PaymentHistory = ({paymentData}) => {
+  return (
+    <Box>
+        
+    </Box>
+  )
+}
+
+export default PaymentHistory
